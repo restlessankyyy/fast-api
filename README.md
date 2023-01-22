2fce21dba365862bc815ab50db46afbbf1dd1919
restlessankyyy <rajankit749@gmail.com>
2023-01-22 15:51:10 +0100

    add relevant updates

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # fast-api
+
+<!-- steps to deploy fast api to google cloud run using terraform
+ -->
+
+
--- hello-api/terraform/variables.tf
+++ hello-api/terraform/variables.tf
@@ -1,26 +1,18 @@
+# variables for main.tf file
+variable "project_id" {
+  description = "The ID of the project in which resources will be provisioned."
+  type        = string
+}
+variable "region" {
+  description = "The region in which resources will be provisioned."
+  type        = string
+}
+variable "service_name" {
+  description = "The name of the Cloud Run service."
+  type        = string
+}
+variable "image_name" {
+  description = "The name of the container image."
+  type        = string
+}
 
-# provider "google" {
-#   project = var.project_id
-#   region = var.region
-# }
-
-# resource "google_container_registry" "registry" {
-#   name = "gcr.io/${var.project_id}"
-# }
-
-# resource "google_cloud_run_service" "service" {
-#   name = var.service_name
-#   location = var.region
-#   template {
-#     spec {
-#       container {
-#         image = "${google_container_registry.registry.name}/${var.image_name}"
-#         resources {
-#           limits {
-#             memory = "1Gi"
-#           }
-#         }
-#       }
-#     }
-#   }
-# }
