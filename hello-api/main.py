da8b19ec44402f81bf864576a9d39fb81160aeac
restlessankyyy <rajankit749@gmail.com>
2023-01-22 13:01:31 +0100

    add fast api init code

---
+++ hello-api/main.py
@@ -0,0 +1,15 @@
+# create a hello world api using fast api
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: str = None):
+    return {"item_id": item_id, "q": q}
+
+# Path: hello-api/requirements.txt'
+# create a requirements.txt file
